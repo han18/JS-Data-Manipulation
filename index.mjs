a449849bd84565c0e19387ab8187ff0ce803c978
han18 <han.fadah@gmail.com>
2023-11-13 20:21:31 -0500

    Data
    Working on findingt the odd number

---
+++ index.mjs
@@ -0,0 +1,54 @@
+// The initial numbers that must be verified.
+const n1 = 10;
+const n2 = 15;
+const n3 = 20;
+const n4 = 5;
+
+// Check one: add up to 50
+// This is a fairly simple operation using
+// arithmetic operators and a comparison.
+const isSum50 = n1 + n2 + n3 + n4 == 50;
+
+const divBy5 = isSum50 / 5;
+console.log(divBy5);
+
+console.log(n1 / 5); //divisible by 5
+console.log(n2 / 5); //divisible by 5
+console.log(n3 / 5); //divisible by 5
+console.log(n4 / 5); //divisible by 5
+
+// console.log(isSum50); // this operation is true
+
+// Check two: at least two odd numbers
+// Here, we use modulus to check if something is odd.
+// Since % 2 is 0 if even and 1 if odd, we can use
+// arithmetic to count the total number of odd numbers.
+const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+
+console.log(isTwoOdd);
+console.log(n1 % 2); // not an odd number
+console.log(n2 % 2); // is an od number
+console.log(n3 % 2);
+
+// Check three: no number larger than 25
+// This time, we use the OR operator to check
+// if ANY of the numbers is larger than 25.
+const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+
+// console.log(isOver25);
+
+// Check four: all unique numbers
+// This is long, and there are more efficient
+// ways of handling it with other data structures
+// that we will review later.
+const isUnique =
+  n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+
+// Here, we put the results into a single variable
+// for convenience. Note how we negate isOver25 using
+// the ! operator. We could also have tested for
+// "isUnder25" as an alternative.
+const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+
+// Finally, log the results.
+console.log(isValid);
